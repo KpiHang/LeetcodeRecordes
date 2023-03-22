926f7b3e45b54565793c611e18fbd80780333e38
KpHang <hangkees@aliyun.com>
2023-03-22 14:58:03 +0800

    LC:96;DP;Golang;

---
+++ 96.不同的二叉搜索树.go
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=96 lang=golang
+ *
+ * [96] 不同的二叉搜索树
+ */
+
+// @lc code=start
+// DP；二叉搜索树是有序的，左<根<右;
+func numTrees(n int) int {
+	dp := make([]int, n+1)
+	dp[0], dp[1] = 1, 1
+	for i := 2; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			dp[i] += dp[j-1] * dp[i-j]
+		}
+	}
+	return dp[n]
+}
+
+// @lc code=end
+
