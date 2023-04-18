5af3ebec68300d2f25f8e0d1953cae9f4b2c15f9
KpHang <hangkees@aliyun.com>
2023-04-18 08:33:57 +0800

    1035;53

---
+++ 1035.不相交的线.go
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=1035 lang=golang
+ *
+ * [1035] 不相交的线
+ */
+
+// @lc code=start
+// 代录-Self core：转化为求最长公共子序列问题；
+func maxUncrossedLines(nums1 []int, nums2 []int) int {
+	m, n := len(nums1), len(nums2)
+	dp := make([][]int, m+1)
+	for i := 0; i <= m; i++ {
+		dp[i] = make([]int, n+1)
+	}
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if nums1[i-1] == nums2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[m][n]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// @lc code=end
+
--- 53.最大子数组和.go
+++ 53.最大子数组和.go
@@ -5,9 +5,24 @@
  */
 
 // @lc code=start
+// 代录：DP法
+func maxSubArray(nums []int) int {
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	// 初始化最大的和
+	mx := nums[0]
+	for i := 1; i < len(nums); i++ {
+		// 会面临2种情况，一个是带前面的和，一个是不带前面的和
+		dp[i] = max(dp[i-1]+nums[i], nums[i])
+		mx = max(mx, dp[i])
+		// fmt.Println(dp)
+	}
+	return mx
+}
+
 // 贪心算法
 // https://leetcode.cn/problems/maximum-subarray/solutions/1536926/by-nehzil-rmeh/?topicTags=greedy
-func maxSubArray(nums []int) int {
+func maxSubArray_贪心(nums []int) int {
 	res := -int(math.Pow(10, 4)) - 1
 	tmp := 0
 	for i := 0; i < len(nums); i++ {
