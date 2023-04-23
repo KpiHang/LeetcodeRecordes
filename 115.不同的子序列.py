141966a2cb898d206c8f72f7d8359bf2fc513fdf
KpHang <hangkees@aliyun.com>
2023-04-23 16:42:47 +0800

    115

---
+++ 115.不同的子序列.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=115 lang=python3
+#
+# [115] 不同的子序列
+#
+
+
+# @lc code=start
+class Solution:
+    # 代录 DP
+    def numDistinct(self, s: str, t: str) -> int:
+        dp = [[0] * (len(t) + 1) for _ in range(len(s) + 1)]
+        # 初始化
+        for i in range(len(s)):
+            dp[i][0] = 1
+        for j in range(1, len(t)):
+            dp[0][j] = 0
+        for i in range(1, len(s) + 1):
+            for j in range(1, len(t) + 1):
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j]
+
+        return dp[-1][-1]
+
+
+# @lc code=end
